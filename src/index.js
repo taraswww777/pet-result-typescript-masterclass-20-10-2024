d2d5e5e908b3f5c60641319b91d814960647bf0a
taraswww777 <taraswww777@mail.ru>
2024-10-20 13:40:24 +0300

    simple js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('hi !!!')
+const copyBook1 = {
+  // type: 'line', 'square',
+  type: 'line',
+  format: 'a4',
+  color: 'white',
+  pageSize: 12,
+}
+
+const copyBook2 = {
+  // type: 'line', 'square',
+  type: 'line',
+  format: 'a4',
+  color: 'white',
+  pageSize: 'двенадцать',
+}
+
+const copyBook3 = {
+  // type: 'line', 'square',
+  type: 'line',
+  format: 'a4',
+  color: 'white',
+  pageSize: 'тридцать два',
+}
+
+function getPageSize(copyBook) {
+  return copyBook.pageSize;
+}
+
+function isBug(copyBook) {
+  return copyBook.pageSize > 12;
+}
+
+console.log('copyBook:', copyBook1)
+
+console.log('copyBook1 getPageSize:', getPageSize(copyBook1));
+console.log('copyBook2 getPageSize:', getPageSize(copyBook2));
+
+console.log('isBug copyBook1:', isBug(copyBook1))
+console.log('isBug copyBook2:', isBug(copyBook2))
+console.log('isBug copyBook3:', isBug(copyBook3))
